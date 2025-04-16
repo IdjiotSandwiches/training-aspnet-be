238865e827463ceb7e4cb5ea0557e691dd1f0d88
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-04-16 14:40:40 +0700

    Create controller

---
+++ Backend/Controllers/STNKController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Backend.Helpers;
+
+namespace Backend.Controllers
+{
+    [EnableCors]
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class STNKController(CrudHelper crudHelper, IMapper mapper) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
